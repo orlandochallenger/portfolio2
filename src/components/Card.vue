25d67fd4575db17b0bed3805473737fbf3422765
orlandofat <orlandofat@gmail.com>
2021-04-22 19:09:14 -0400

    fix src image

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,15 @@
 <template>
+<div class="gallery">
     <div class="card" v-for="project in projects" :key="project.id">
-        <img :src="project.image" style="width:100%;" :alt="project.title">
+        <img :alt="project.title" :src="project.src" style="width:100%;" />
         <div class="container">
             <h2>{{ project.title }}</h2>
             <p>{{ project.desc }}</p>
             <button class="button"><a :href="project.url" target="_blank">View more</a></button>
         </div>
-    </div>     
+    </div> 
+</div>
+        
 </template>
 
 <script>
@@ -17,35 +20,35 @@ export default {
             projects: [
                 {
                     id: 1,
-                    image: "../assets/img/e_commerce.png",
+                    src: require("../assets/img/e_commerce.png"),
                     title: "E-Commerce Site",
                     desc: "Created an e-commerce site with react and commerce.js",
                     url: "https://appleman-e-commerce.netlify.app/"
                 },
                 {
                     id: 2,
-                    image: "../assets/img/employeeDirectory.png",
+                    src: require("../assets/img/employeeDirectory.png"),
                     title: "Employee Directory",
                     desc: "Using HTML, CSS and JavaScript to create an employee directory by communicating with a third-party API",
                     url: "https://orlandochallenger.github.io/project-8/"
                 },
                 {
                     id: 3,
-                    image: "../assets/img/WheelOfSucess.png",
+                    src: require("../assets/img/WheelOfSucess.png"),
                     title: "Game Show App",
                     desc: "In this project, I create a browser version of “Wheel of Success”, a word guessing game using HTML, CSS and JavaScript.",
                     url: "https://orlandochallenger.github.io/project-6/"
                 },
                 {
                     id: 4,
-                    image: "../assets/img/gallery_mockup.png",
+                    src: require("../assets/img/gallery_mockup.png"),
                     title: "Photo Gallery",
                     desc: "An Interactive Photo Gallery using HTML, CSS and JavaScript.",
                     url: "https://orlandochallenger.github.io/project-5/"
                 },
                 {
                     id: 5,
-                    image: "../assets/img/Circle-UI-Kit.png",
+                    src: require("../assets/img/Circle-UI-Kit.png"),
                     title: "Web Style Guide",
                     desc: "Web Style Guide using HTML, CSS and Sass as an important tool in a modern Front End Web Developer’s toolbox.",
                     url: "https://orlandochallenger.github.io/project-4/"
@@ -108,4 +111,27 @@ a{
   box-shadow: 0 5px #666;
   transform: translateY(4px);
 }
+
+/* .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-template-rows: auto;
+    gap: 20px;
+} */
+
+.gallery {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center; 
+}
+
+/* @media screen and (min-width: 768px) {
+.gallery {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center; 
+}
+} */
 </style>
