5f7756a67e840b481e9b2a5b0317b159e9527170
orlandofat <orlandofat@gmail.com>
2021-05-24 16:34:31 -0400

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -20,4 +20,9 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  document.title = `${ process.env.VUE_APP_TITLE } - ${ to.name }`
+  next()
+})
+
 export default router
